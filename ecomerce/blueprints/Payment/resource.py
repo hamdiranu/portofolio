2936cb21d97c4c0dd660f250db72eb616a574bbe
Hamdi Ranuharja <hamdi@alterra.id>
2020-01-04 23:59:47 +0700

    coba payment

--- ecomerce/blueprints/Payment/resource.py
+++ ecomerce/blueprints/Payment/resource.py
@@ -3,14 +3,14 @@ from flask_restful import Api, reqparse, marshal, Resource, inputs
 from sqlalchemy import desc
 from . import *
 
-from .model import Checkouts
+from .model import Payments
 from blueprints.Cart.model import Carts
 
 from flask_jwt_extended import JWTManager, verify_jwt_in_request, get_jwt_claims, jwt_required
 from blueprints import db,app, admin_required
 
-bp_checkout = Blueprint('checkout', __name__)
-api = Api(bp_checkout)
+bp_payment = Blueprint('payment', __name__)
+api = Api(bp_payment)
 
 
 ################################################
@@ -18,7 +18,7 @@ api = Api(bp_checkout)
 ################################################
 
 
-class CheckoutResource(Resource):
+class PaymentResource(Resource):
 
     def __init__(self):
         pass
@@ -26,53 +26,54 @@ class CheckoutResource(Resource):
     @jwt_required
     @admin_required
     def get(self, id):
-        qry = Checkouts.query.get(id)
+        qry = Payments.query.get(id)
         if qry is not None and qry.deleted == False:
-            return marshal(qry, Checkouts.response_fields), 200
+            return marshal(qry, Payments.response_fields), 200
         return {'status':'NOT_FOUND'}, 404
 
     @jwt_required
     def post(self):
 
         parser = reqparse.RequestParser()
-        parser.add_argument('nama_penerima', location = 'json')
-        parser.add_argument('alamat', location = 'json')
-        parser.add_argument('kode_pos', location = 'json')
-        parser.add_argument('nomor_telepon', location = 'json')
-        parser.add_argument('metode_pengiriman', location = 'json')
+        parser.add_argument('cardholder', location = 'json')
+        parser.add_argument('card_number', location = 'json')
+        parser.add_argument('security_code', location = 'json')
+        parser.add_argument('expired_month', location = 'json')
+        parser.add_argument('expired_year', location = 'json')
+        parser.add_argument('status_cod', location = 'json')
         args = parser.parse_args()
 
         verify_jwt_in_request()
         claims = get_jwt_claims()
         qry_cart = Carts.query.filter_by(user_id = claims["id"]).filter_by(status = False).first()
-        qry_checkout = Checkouts.query.filter_by(cart_id = qry_cart.id).first()
+        qry_payment = Payments.query.filter_by(cart_id = qry_cart.id).first()
 
-        if qry_checkout == None:
+        if qry_payment == None:
             cart_id = qry_cart.id
             jumlah_barang = qry_cart.total_item
             total_harga = qry_cart.total_harga
 
-            checkout = Checkouts(cart_id, args['nama_penerima'], args['alamat'], args['kode_pos'],
-                        args['nomor_telepon'], args['metode_pengiriman'], jumlah_barang, total_harga)
-            db.session.add(checkout)
+            payment = Payments(cart_id, args['cardholder'], args['card_number'], args['security_code'],
+                        args['expired_month'], args['status_cod'], jumlah_barang, total_harga)
+            db.session.add(payment)
             db.session.commit()
 
-            app.logger.debug('DEBUG : %s', checkout)
+            app.logger.debug('DEBUG : %s', payment)
 
-            return marshal(checkout, Checkouts.response_fields), 200, {'Content-Type' : 
+            return marshal(payment, Payments.response_fields), 200, {'Content-Type' : 
             'application/json' }
 
         else :
-            qry_checkout.nama_penerima = args["nama_penerima"]
-            qry_checkout.alamat = args["alamat"]
-            qry_checkout.kode_pos = args["kode_pos"]
-            qry_checkout.nomor_telepon = args["nomor_telepon"]
-            qry_checkout.metode_pengiriman = args["metode_pengiriman"]
-            qry_checkout.jumlah_barang = qry_cart.total_item
-            qry_checkout.total_harga = qry_cart.total_harga
+            qry_payment.cardholder = args["cardholder"]
+            qry_payment.card_number = args["card_number"]
+            qry_payment.security_code = args["security_code"]
+            qry_payment.expired_month = args["expired_month"]
+            qry_payment.status_cod = args["status_cod"]
+            qry_payment.jumlah_barang = qry_cart.total_item
+            qry_payment.total_harga = qry_cart.total_harga
             db.session.commit()
 
-            return marshal(qry_checkout, Checkouts.response_fields), 200, {'Content-Type' : 'application/json' }
+            return marshal(qry_payment, Payments.response_fields), 200, {'Content-Type' : 'application/json' }
     
     @jwt_required
     @admin_required
@@ -92,7 +93,7 @@ class CheckoutResource(Resource):
         jumlah_barang = qry_cart.total_item
         total_harga = qry_cart.total_harga
 
-        qry = Checkouts.query.get(id)
+        qry = Payments.query.get(id)
 
         if qry is None:
             return {'status': 'NOT_FOUND'}, 404
@@ -108,12 +109,12 @@ class CheckoutResource(Resource):
 
         db.session.commit()
 
-        return marshal(qry, Checkouts.response_fields), 200, {'Content-Type' : 'application/json' }
+        return marshal(qry, Payments.response_fields), 200, {'Content-Type' : 'application/json' }
 
     @jwt_required
     @admin_required
     def delete(self,id):
-        qry = Checkouts.query.get(id)
+        qry = Payments.query.get(id)
 
         if qry is None:
             return {'status': 'NOT_FOUND'}, 404
@@ -128,7 +129,7 @@ class CheckoutResource(Resource):
     def patch(self):
         return 'Not yet implement', 501
 
-class CheckoutList(Resource):
+class PaymentList(Resource):
 
     def __init__(self):
         pass
@@ -147,7 +148,7 @@ class CheckoutList(Resource):
 
         offset = (args['p'] * args['rp']) - args['rp']
 
-        qry = Checkouts.query
+        qry = Payments.query
 
         if args['user_id'] is not None:
             qry = qry.filter_by(user_id = args['user_id'])
@@ -155,14 +156,14 @@ class CheckoutList(Resource):
         if args['orderby'] is not None :
             if args['orderby'] == 'user_id':
                 if args['sort'] == 'desc':
-                    qry = qry.order_by(desc(Checkouts.user_id))
+                    qry = qry.order_by(desc(Payments.user_id))
                 else:
-                    qry = qry.order_by(Checkouts.user_id)
+                    qry = qry.order_by(Payments.user_id)
         rows = []
         for row in qry.limit(args['rp']).offset(offset).all():
             if row is not None and row.deleted == False:
-                rows.append(marshal(row,Checkouts.response_fields))
+                rows.append(marshal(row,Payments.response_fields))
         return rows, 200
 
-api.add_resource(CheckoutList, '', '/list')
-api.add_resource(CheckoutResource, '', '/<id>')
+api.add_resource(PaymentList, '', '/list')
+api.add_resource(PaymentResource, '', '/<id>')
