5e8ab547faf7bdecd2781f83dafd5e9a491300b3
Hamdi Ranuharja <hamdi@alterra.id>
2020-01-05 09:00:00 +0700

    payment done

--- ecomerce/blueprints/Payment/resource.py
+++ ecomerce/blueprints/Payment/resource.py
@@ -5,6 +5,7 @@ from . import *
 
 from .model import Payments
 from blueprints.Cart.model import Carts
+from blueprints.Checkout.model import Checkouts
 
 from flask_jwt_extended import JWTManager, verify_jwt_in_request, get_jwt_claims, jwt_required
 from blueprints import db,app, admin_required
@@ -40,76 +41,33 @@ class PaymentResource(Resource):
         parser.add_argument('security_code', location = 'json')
         parser.add_argument('expired_month', location = 'json')
         parser.add_argument('expired_year', location = 'json')
-        parser.add_argument('status_cod', location = 'json')
+        parser.add_argument('status_cod', type=inputs.boolean, location = 'json')
         args = parser.parse_args()
 
         verify_jwt_in_request()
         claims = get_jwt_claims()
         qry_cart = Carts.query.filter_by(user_id = claims["id"]).filter_by(status = False).first()
-        qry_payment = Payments.query.filter_by(cart_id = qry_cart.id).first()
+        qry_checkout = Checkouts.query.filter_by(cart_id = qry_cart.id).first()
+        
 
-        if qry_payment == None:
-            cart_id = qry_cart.id
-            jumlah_barang = qry_cart.total_item
-            total_harga = qry_cart.total_harga
+        # Mendefinisikan nilai yang diambil dari table lain
+        checkout_id = qry_checkout.id
+        # jumlah_barang = qry_checkout.jumlah_barang
+        # total_harga = qry_checkout.total_harga
 
-            payment = Payments(cart_id, args['cardholder'], args['card_number'], args['security_code'],
-                        args['expired_month'], args['status_cod'], jumlah_barang, total_harga)
-            db.session.add(payment)
-            db.session.commit()
-
-            app.logger.debug('DEBUG : %s', payment)
-
-            return marshal(payment, Payments.response_fields), 200, {'Content-Type' : 
-            'application/json' }
-
-        else :
-            qry_payment.cardholder = args["cardholder"]
-            qry_payment.card_number = args["card_number"]
-            qry_payment.security_code = args["security_code"]
-            qry_payment.expired_month = args["expired_month"]
-            qry_payment.status_cod = args["status_cod"]
-            qry_payment.jumlah_barang = qry_cart.total_item
-            qry_payment.total_harga = qry_cart.total_harga
-            db.session.commit()
-
-            return marshal(qry_payment, Payments.response_fields), 200, {'Content-Type' : 'application/json' }
-    
-    @jwt_required
-    @admin_required
-    def put(self, id):
-
-        parser = reqparse.RequestParser()
-        parser.add_argument('cart_id', location = 'json', required = True)
-        parser.add_argument('nama_penerima', location = 'json')
-        parser.add_argument('alamat', location = 'json')
-        parser.add_argument('kode_pos', location = 'json')
-        parser.add_argument('nomor_telepon', location = 'json')
-        parser.add_argument('metode_pengiriman', location = 'json')
-        args = parser.parse_args()
-
-        # Untuk mengambil nilai jumlah barang & total harga
-        qry_cart = Carts.query.get(id)
-        jumlah_barang = qry_cart.total_item
-        total_harga = qry_cart.total_harga
-
-        qry = Payments.query.get(id)
-
-        if qry is None:
-            return {'status': 'NOT_FOUND'}, 404
-
-        qry.cart_id = args['cart_id']
-        qry.nama_penerima = args['nama_penerima']
-        qry.alamat = args['alamat']
-        qry.kode_pos = args['kode_pos']
-        qry.nomor_telepon = args['nomor_telepon']
-        qry.metode_pengiriman = args['metode_pengiriman']
-        qry.jumlah_barang = jumlah_barang
-        qry.total_harga = total_harga
-
+        # Membuat table payment
+        payment = Payments(checkout_id, args['cardholder'], args['card_number'], args['security_code'],
+                    args['expired_month'], args['expired_year'], args['status_cod'])
+        db.session.add(payment)
         db.session.commit()
 
-        return marshal(qry, Payments.response_fields), 200, {'Content-Type' : 'application/json' }
+        qry_cart.status = True
+        db.session.commit()
+
+        app.logger.debug('DEBUG : %s', payment)
+
+        return marshal(payment, Payments.response_fields), 200, {'Content-Type' : 
+        'application/json' }
 
     @jwt_required
     @admin_required
