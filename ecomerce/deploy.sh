263a89a323c8801ca674183ea29681b8b408bc8a
Hamdi Ranuharja <hamdi@alterra.id>
2020-01-22 09:50:25 +0700

    push deploy.sh

---
+++ ecomerce/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
+cd /home/admin/var
+git pull
+
+source ~/.profile
+echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+docker stop testchallenge
+docker rm testchallenge
+docker rmi hamdiranu/containerd:be2
+docker run -d --name testchallenge -p 5000:5000 hamdiranu/containerd:be2
